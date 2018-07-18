bbd929d5380c935550631aca66b720ce8f877ea1
Mi&Jack <mijackstudio@gmail.com>
2018-07-18 23:32:05 +0800

    修改empty-log的内容，交由xposed记录日志

--- lib/src/main/java/com/mijack/XlogBuilder.java
+++ lib/src/main/java/com/mijack/XlogBuilder.java
@@ -31,70 +31,13 @@ public class XlogBuilder {
     }
 
     public static void logMethodEnterInfo(int hookId, MethodType methodType, String methodSign, Object instance, Object... args) {
-        int pid = XlogUtils.getProcessId();
-        int threadId = XlogUtils.getCurrentThreadId();
-
-        StringBuilder sb = new StringBuilder("{").append(String.format(KEY_TO_VALUE, "logType", LOG_TYPE_ENTER));
-        sb.append(",").append(String.format(KEY_TO_VALUE, "time", XlogUtils.currentTime()));
-        sb.append(",").append(String.format(KEY_TO_VALUE, "processName", XlogUtils.getProcessName()));
-        sb.append(",").append(String.format(KEY_TO_VALUE, "threadName", XlogUtils.getCurrentThreadInfo()));
-        sb.append(",").append(String.format(KEY_TO_VALUE, "pid", pid));
-        if (hookId > 0) {
-            sb.append(",").append(String.format(KEY_TO_VALUE, "hookId", hookId));
-        }
-        XlogUtils.appendInvokeLine(hookId, sb);
-        sb.append(",").append(String.format(KEY_TO_VALUE, "methodType", methodType.typename()));
-        sb.append(",").append(String.format(KEY_TO_VALUE, "methodSign", methodSign));
-//判断是否是方法开始
-        if (methodType.hasInstance()) {
-            sb.append(",").append(String.format(KEY_TO_VALUE2, "instance", XlogUtils.object2String(instance)));
-        }
-        sb.append(",").append(XlogUtils.paramsToString(args));
-        sb.append("}");
-        LogWriter.d(hookId, pid, threadId, sb.toString());
+        // hook by xposed
     }
 
     public static void logMethodExitInfo(int hookId, MethodType methodType, String methodSign,
                                          Object instance, MethodExecuteResultType resultType, Object result, Throwable throwable) {
-
-        int pid = XlogUtils.getProcessId();
-        int threadId = XlogUtils.getCurrentThreadId();
-
-        StringBuilder sb = new StringBuilder("{").append(String.format(KEY_TO_VALUE, "logType", LOG_TYPE_EXIT));
-        sb.append(",").append(String.format(KEY_TO_VALUE, "time", XlogUtils.currentTime()));
-        sb.append(",").append(String.format(KEY_TO_VALUE, "processName", XlogUtils.getProcessName()));
-        sb.append(",").append(String.format(KEY_TO_VALUE, "threadName", XlogUtils.getCurrentThreadInfo()));
-        sb.append(",").append(String.format(KEY_TO_VALUE, "pid", pid));
-        XlogUtils.appendInvokeLine(hookId, sb);
-        if (hookId > 0) {
-            sb.append(",").append(String.format(KEY_TO_VALUE, "hookId", hookId));
-        }
-        sb.append(",").append(String.format(KEY_TO_VALUE, "methodType", methodType.typename()));
-        sb.append(",").append(String.format(KEY_TO_VALUE, "methodSign", methodSign));
-//判断是否是方法开始
-        if (methodType.hasInstance()) {
-            sb.append(",").append(String.format(KEY_TO_VALUE2, "instance", XlogUtils.object2String(instance)));
-        }
-        switch (resultType) {
-            case HAS_RESULT:
-                sb.append(",").append(String.format(KEY_TO_VALUE2, "result", XlogUtils.object2String(result)));
-                break;
-            case HAS_THROWABLE:
-                sb.append(",").append(String.format(KEY_TO_VALUE, "throwable", XlogUtils.object2String(throwable)));
-                break;
-            default:
-            case NO_THING:
-                // do nothing
-                break;
-        }
-        sb.append("}");
-        LogWriter.d(hookId, pid, threadId, sb.toString());
+        // hook by xposed
     }
 
 
-    public static final String KEY_TO_VALUE = "\"%s\":\"%s\"";
-    public static final String KEY_TO_VALUE2 = "\"%s\":%s";
-
-    public static final String LOG_TYPE_ENTER = "enter";
-    public static final String LOG_TYPE_EXIT = "exit";
 }
